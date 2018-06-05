9652ac3e62b1c0c1ea20039781c2de80f4555b36
Borja Sanchidrian <s7k.yuuki@gmail.com>
2018-06-05 19:36:00 +0200

    Parse rss

--- verificacion/views.py
+++ verificacion/views.py
@@ -1,23 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import feedparser
 
 from .forms import InputForm
 from .strings_counter import StringsCounter
 
 def index(request):
-    dic = None
-    if request.method == 'POST':
-        form = InputForm(request.POST)
-        if form.is_valid():
-            # TODO
-            input_received = form.cleaned_data['input']
-            print(type(input_received))
-            sc = StringsCounter()
-            dic = sc.count_strings(input_received)
-            print(dic)
+    text = ""
+    link = "http://ep00.epimg.net/rss/elpais/portada.xml"
+    feed = feedparser.parse(link)
+    print(feed["channel"]["title"])
+
+    for item in feed["items"]:
+        text += item["title"]
+        text += " "
+        text += item["description"] 
+        # text += item["content:encoded"]
+        try:
+            text += " "
+            text += item.content        
+        except Exception:
+            pass
+        # print(item["title"])
+        # print(item["description"])
+        # print("\n\n")
+
+
+    sc = StringsCounter()
+    dic = sc.count_strings(text)
+    # print(dic)
 
     context = {
-        'palabras': dic,
-        'form': InputForm()
+        'palabras': dic
     }
     return render(request, 'verificacion/index.html', context)
