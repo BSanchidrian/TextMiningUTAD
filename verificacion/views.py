6c6e1fb7ff4f222d1bf65afb11ead7c6a9802be8
Borja Sanchidrian <s7k.yuuki@gmail.com>
2018-06-06 19:40:56 +0200

    Html

--- verificacion/views.py
+++ verificacion/views.py
@@ -1,46 +1,44 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import urllib.request
-
-from html.parser import HTMLParser
-
-class MyHTMLParser(HTMLParser):
-
-    def init(self):
-        self.text = ""
-
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
-        self.text += str(data)
-        self.text += " "
-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from bs4.element import Comment 
 from .forms import InputForm
 from .strings_counter import StringsCounter
 
+def tag_visible(element):
+    if element.parent.name in ['p', 'a', 'span']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts) 
+    return u" ".join(t.strip() for t in visible_texts)
+
 def index(request):
     dic = None
+    text = ""
+
     if request.method == 'POST':
         form = InputForm(request.POST)
         if form.is_valid():
             input_received = form.cleaned_data['input']
-            print(type(input_received))
-
+            date = form.cleaned_data['date']
+            print(date)
             link = input_received
-            f = urllib.request.urlopen(link)
-            text = f.read()
-            parser = MyHTMLParser()
-            parser.init()
-            parser.feed(str(text))
+            html = urlopen(link)
+            soup = BeautifulSoup(html,'html.parser')
+
+            for tag in soup.findAll('p'):
+                text += " " + str(tag.get_text())
 
             sc = StringsCounter()
-            dic = sc.count_strings(parser.text)
-            # print(dic)
+            dic = sc.count_strings(text)
 
     context = {
         'palabras': dic,
